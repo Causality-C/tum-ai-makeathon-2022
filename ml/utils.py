6eb780fc9e3e0376284162e98ffe0b2cab5e0c54
Paul Delseith <delseith@tufast.de>
2022-10-01 20:14:35 -0500

    minor fix to model prediction function

--- ml/utils.py
+++ ml/utils.py
@@ -1,19 +1,25 @@
 import torch
 import torchvision
 
+
 def load_model(path):
-    model = torch.load(path)
+    device = torch.device("cpu")
+    model = torch.load(path, map_location=device)
     return model
+
 
 def inference_single_image(path, image):
     model = load_model(path)
-    transform = torchvision.transforms.Compose([
-            torchvision.transforms.ToPILImage(),
+    transform = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.ToTensor(),
             # transforms.CenterCrop(320),
             torchvision.transforms.Resize((320, 320)),
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.ConvertImageDtype(torch.float32)
-            ])
+            torchvision.transforms.ConvertImageDtype(torch.float32),
+        ]
+    )
     input = transform(image)
+    if len(input.shape) < 4:
+        input = torch.unsqueeze(input, dim=0)
     prediction = model(input)
     return prediction
